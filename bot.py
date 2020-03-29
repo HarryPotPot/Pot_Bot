1b41cf4291d718164290658e79a6c5c340b589b5
HarryPotPot <chaukinyi1029@gmail.com>
2020-03-29 17:46:16 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,6 +12,14 @@ bot = commands.Bot(command_prefix = '/')
 async def on_ready():
     print(">> Bot is online <<")
 
+@bot.event
+async def on_member_join(member):
+    print(f'{member} joined!')
+
+@bot.event
+async def on_member_remove(member):
+    print(f'{member} left!')
+
 @bot.command()
 async def picture(ctx):
     random_pic = random.choice(jdata['url_pic'])

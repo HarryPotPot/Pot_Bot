91ed20eeae6fecb79755d1eca77902e7f174576a
HarryPotPot <chaukinyi1029@gmail.com>
2020-03-29 17:44:20 +0800

    Change

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import json
+import random
 
 with open('setting.json', 'r', encoding = 'utf8') as jfile:
     jdata = json.load(jfile)
@@ -11,19 +12,10 @@ bot = commands.Bot(command_prefix = '/')
 async def on_ready():
     print(">> Bot is online <<")
 
-@bot.event
-async def on_member_join(member):
-    channel = bot.get_channel(int(jdata['Wel_Left_Channel']))
-    await channel.send(f'{member} joined!')
-
-@bot.event
-async def on_member_remove(member):
-    channel = bot.get_channel(int(jdata['Wel_Left_Channel']))
-    await channel.send(f'{member} left!')
-
-
 @bot.command()
-async def ping(ctx):
-    await ctx.send(f'{round(bot.latency * 1000)} ms')
+async def picture(ctx):
+    random_pic = random.choice(jdata['url_pic'])
+    await ctx.send(random_pic)
+
 
 bot.run(jdata['TOKEN'])
